9c084418235e88e2356b2b32be02dda14c28371d
xiaoshihou <xiaoshihou@tutamail.com>
2025-11-09 10:47:21 +0000

    feat: add giscus comment

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,9 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import { useData, useRoute } from "vitepress";
+import { toRefs } from "vue";
+import giscusTalk from "vitepress-plugin-comment-with-giscus";
 import "./style.css";
 import "./custom.css";
 
@@ -14,5 +17,37 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
+
+    // Obtain configuration from: https://giscus.app/
+    giscusTalk(
+      {
+        repo: "xiaoshihou514/xiaoshihou514.github.io",
+        repoId: "R_kgDOMLz41w",
+        category: "Announcements",
+        categoryId: "DIC_kwDOMLz4184CxmYO",
+        mapping: "pathname",
+        inputPosition: "bottom",
+        lang: "zh-CN",
+        locales: {
+          zh: "zh-CN",
+          en: "en",
+        },
+        homePageShowComment: false,
+        lightTheme: "preferred_color_scheme",
+        darkTheme: "preferred_color_scheme",
+        strict: false,
+        reactionsEnabled: true,
+      },
+      {
+        frontmatter,
+        route,
+      },
+      false,
+    );
   },
 } satisfies Theme;
